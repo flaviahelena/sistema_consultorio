d2bdb0ae9018ecae4148fa03ea711d6ce5584a37
Flavia Helena da Silva <flaviahsilcor@gmail.com>
2023-01-09 11:25:33 -0300

    Insert class Client and dentist

---
+++ br.com.projeto.model/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistema_consultorio.br.com.projeto.model
+{
+    public class Cliente : Pessoa
+    {
+        public Image foto { get; set; }
+        public string nome_pai { get; set; }
+        public string nome_mae { get; set; }
+    }
+}
---
+++ br.com.projeto.model/Dentista.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistema_consultorio.br.com.projeto.model
+{
+    public class Dentista : Pessoa
+    {
+        public int cro { get; set; }
+        public string uf_cro { get; set; }
+    }
+}
