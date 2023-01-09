bea76a1930385c1f572a4f8ec0c057cabb5f8df0
Flavia Helena da Silva <flaviahsilcor@gmail.com>
2023-01-09 11:28:47 -0300

    System changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
+using sistema_consultorio.br.com.projeto.view;
+
 namespace sistema_consultorio
 {
-    internal static class Program
+     static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
@@ -8,10 +10,12 @@ namespace sistema_consultorio
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new frmLogin());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmFuncionario());
+
+            //ApplicationConfiguration.Initialize();
+            //Application.Run(new frmLogin());
         }
     }
 }
