e83ccde1e8c46a190aed78d92da1ffef4c9f5088
Flavia Helena da Silva <flaviahsilcor@gmail.com>
2023-01-10 22:11:55 -0300

    Create viwes to calendar

---
+++ br.com.projeto.view/frmEvento.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace sistema_consultorio.br.com.projeto.view
+{
+    partial class frmEvento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAgendar = new System.Windows.Forms.Button();
+            this.txtProcedimento = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(75, 41);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(261, 23);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(75, 103);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(125, 23);
+            this.txtData.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(211, 103);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 23);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome do Paciente:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Data:";
+            // 
+            // btnAgendar
+            // 
+            this.btnAgendar.Location = new System.Drawing.Point(234, 249);
+            this.btnAgendar.Name = "btnAgendar";
+            this.btnAgendar.Size = new System.Drawing.Size(102, 26);
+            this.btnAgendar.TabIndex = 6;
+            this.btnAgendar.Text = "Agendar";
+            this.btnAgendar.UseVisualStyleBackColor = true;
+            this.btnAgendar.Click += new System.EventHandler(this.btnAgendar_Click);
+            // 
+            // txtProcedimento
+            // 
+            this.txtProcedimento.Location = new System.Drawing.Point(75, 158);
+            this.txtProcedimento.Multiline = true;
+            this.txtProcedimento.Name = "txtProcedimento";
+            this.txtProcedimento.Size = new System.Drawing.Size(261, 76);
+            this.txtProcedimento.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Procedimento:";
+            // 
+            // frmEvento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 285);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtProcedimento);
+            this.Controls.Add(this.btnAgendar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtNome);
+            this.Name = "frmEvento";
+            this.Text = "frmEvento";
+            this.Load += new System.EventHandler(this.frmEvento_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtNome;
+        private TextBox txtData;
+        private TextBox textBox4;
+        private Label label1;
+        private Label label2;
+        private Button btnAgendar;
+        private TextBox txtProcedimento;
+        private Label label3;
+    }
+}
